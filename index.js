5ee176e2b6e37dc1fb7696b027fdc4bd65884485
simee56 <pandeysimee7@gmail.com>
2025-01-07 18:16:23 +0530

    PATCH method

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 8000;
 
 //  MIDDLEWARE
 app.use(express.json());
-app.use(express.urlencoded({extended :false}));
+app.use(express.urlencoded({ extended: false }));
 
 //GET METHOD 
 app.get('/api/usersData', (req, res) => {
@@ -20,18 +20,42 @@ app.get('/api/usersData', (req, res) => {
 app.get('/api/usersData/:id', (req, res) => {
     const userId = Number(req.params.id);
     const userData = usersData.find((userData) => userData.id === userId);
+    // Check if the user exists
+    if (!userData) {
+        return res.status(404).json({ status: "error", message: "User not found" });
+    }
     return res.json(userData);
 })
 
 
 //POST METHOd
-app.post('/api/usersData', (req, res) =>{
+app.post('/api/usersData', (req, res) => {
     const body = req.body;
-    usersData.push({...body, id : usersData.length + 1});
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(usersData), (err,data) =>{
-        return res.json({status : "success"});
+    usersData.push({ ...body, id: usersData.length + 1 });
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(usersData), (err, data) => {
+        return res.json({ status: "success" });
     })
 })
+
+
+//PATCH METHOD 
+app.patch('/api/usersData/:id', (req, res) => {
+    const userId = Number(req.params.id);
+    const userData = usersData.find((userData) => userData.id === userId);
+    // Check if the user exists
+    if (!userData) {
+        return res.status(404).json({ status: "error", message: "User not found" });
+    }
+    // Update the user with new data
+    Object.assign(userData, req.body);
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(usersData), (err, data) => {
+        if(err){
+            return res.status(500).json({ status : "Error",message :"Error detected", err})
+        }
+        return res.status(200).json({ status :"success", data : userData});
+    });
+});
 
 
 app.listen(PORT, () => {
