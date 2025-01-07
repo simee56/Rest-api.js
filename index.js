b079addc6724a189d5f1a817b9b1cbc12af591e1
simee56 <pandeysimee7@gmail.com>
2025-01-07 18:28:17 +0530

    DELETE method

--- index.js
+++ index.js
@@ -50,12 +50,31 @@ app.patch('/api/usersData/:id', (req, res) => {
     Object.assign(userData, req.body);
 
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(usersData), (err, data) => {
-        if(err){
-            return res.status(500).json({ status : "Error",message :"Error detected", err})
+        if (err) {
+            return res.status(500).json({ status: "Error", message: "Error detected", err })
         }
-        return res.status(200).json({ status :"success", data : userData});
+        return res.status(200).json({ status: "success", data: userData });
     });
 });
+
+
+//DELETE METHOD
+app.delete('/api/usersData/:id', (req, res) => {
+    const userId = Number(req.params.id);
+    const userData = usersData.findIndex((userData) => userData.id === userId);
+      // Check if the user exists
+      if (!userData) {
+        return res.status(404).json({ status: "error", message: "User not found" });
+    }
+        // Remove the user from the array
+        const deletedUser = usersData.splice(userData, 1)[0];
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(usersData), (err, data) => {
+        if (err) {
+            return res.status(500).json({ status: "Error", message: "Error detected", err })
+        }
+        return res.status(200).json({ status: "success", data: deletedUser });
+    });
+})
 
 
 app.listen(PORT, () => {
